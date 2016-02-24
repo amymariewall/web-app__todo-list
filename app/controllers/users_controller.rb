ccac7c9240740847ce5f1a5bd799d92a7ba76fc6
Amy Wall <amymariewall@gmail.com>
2016-02-24 12:00:51 -0600

    User Create Done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,1 +1,15 @@
 # This controller is for all the CRUD operations related to a User.
+
+MyApp.get "/" do
+  erb :"users/add"
+end
+
+MyApp.get "/user/add" do
+  erb :"users/add"
+end
+
+MyApp.post "/user/create" do
+  @user = User.new(name: params["name"], email: params["email"], password: params["password"])
+  @user.save
+  erb :"users/added"
+end

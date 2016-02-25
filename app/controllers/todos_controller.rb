3830c4ea3597725032fafca42136ca9b7b8b0ac0
Amy Wall <amymariewall@gmail.com>
2016-02-24 18:03:43 -0600

    Making Update for Todos that is a checkbox where you can check off all the tasks that have been completed

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,1 +1,19 @@
 # This controller is for all the CRUD operations related to a Todo.
+
+MyApp.get "/todo/add" do
+  @users = User.all
+  erb :"todos/add"
+end
+
+MyApp.post "/todo/create" do
+  todo = Todo.new(title: params["title"], description: params["description"], user_id: params["user_id"], completed: false)
+  todo.save
+  @message = "Successfully created #{todo.title} and assigned it to #{todo.user_name}!"
+  binding.pry
+  erb :"todos/added"  
+end
+
+MyApp.get "/todos/view" do 
+    @todos = Todo.all
+    erb :"todos/view_all"
+  end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,9 @@
 class Todo < ActiveRecord::Base
+
+  def user_name
+    x = User.find_by_id(self.user_id)
+    y = x.name
+    return y
+  end
+
 end

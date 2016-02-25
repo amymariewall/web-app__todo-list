308ed06a2cc06a745ec64fda1243501985be4152
Amy Wall <amymariewall@gmail.com>
2016-02-24 18:23:53 -0600

    Checkbox to check off completed activities is created

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -17,3 +17,11 @@ MyApp.get "/todos/view" do
     @todos = Todo.all
     erb :"todos/view_all"
   end
+
+MyApp.post "/todos/update" do
+  todos = Todo.where("id" => params["todos"])
+  todos.update_all(completed: true)
+  @message = "Great work completing #{todos}!"
+  binding.pry
+  erb :"todos/added"
+end

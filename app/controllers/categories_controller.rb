d5f344d5f582cead64d75962e92f58b8be828fa6
Amy Wall <amymariewall@gmail.com>
2016-02-26 14:10:36 -0600

    Added CR for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,16 +2,22 @@
 
 MyApp.get "/category/add" do
   @todos = Todo.all
-  erb :"category/add"  
+  erb :"categories/add"  
 end
 
 MyApp.post "/category/create" do
   @category = Category.new(category_name: params["category_name"])
+  binding.pry
   @category.save
   @todos = Todo.where({"id" => params["todos"]})
   @todos.update_all({"category_id" => @category.id})
   @categories = Category.all
   # Will ultimately want this to redirect to view ONE category page
+  redirect "/categories/view"
+end
+
+MyApp.get "/categories/view" do 
+  @categories = Category.all
   erb :"categories/view"
 end
 
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,16 @@
 class Category < ActiveRecord::Base
 
+  def set_todos
+    @todos = []
+    todos = Todo.where({"category_id" => self.id})
+    todos.each do |t| 
+      @todos <<  t.title
+    end
+  end
+
+  def get_todos
+    self.set_todos
+    return @todos
+  end
+
 end
